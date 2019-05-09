36c388f1a707cc97e33d32301e9400022b2ffa2a
Nick Caris <nickcaris97@gmail.com>
2019-05-09 11:39:54 +0200

    Init and first test

---
+++ src/main/java/asd/slack/Caris/Slackapp/SlackAppApplication.java
@@ -0,0 +1,13 @@
+package asd.slack.Caris.Slackapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlackAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/asd/slack/Caris/Slackapp/SlackAppApplicationTests.java
@@ -0,0 +1,16 @@
+package asd.slack.Caris.Slackapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SlackAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
